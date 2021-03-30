286b03cf3c051e9e28057a3680d74bb3bec11dc8
Oleh Hordon <olehhordon@Olehs-MacBook-Pro.local>
2021-03-31 00:36:41 +0300

    add terraform files

---
+++ terr/variables.tf
@@ -0,0 +1,21 @@
+variable access_key {}
+
+variable secret_key {}
+
+variable key_name {}
+
+variable vpcCIDRblock {}
+
+variable region {}
+
+variable "ami" {}
+
+variable "instance_type" {}
+
+variable "destinationCIDRblock" {}
+
+variable "CIDRblock" {}
+
+variable subnetCIDRblock {}
+
+variable projectName {}
